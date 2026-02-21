4826ce468447bad67ab7f05b8247e6574eaff607
Ayaan Syed <ayaanplayz18@gmail.com>
2026-02-21 15:01:23 +0530

    Minor Name Change

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Reze",
+  title: "REZE | Ai",
   description: "Personal Assistant",
   icons: {
     icon: '/logo.png',
